eee7f47ff5f9cae37c0079d56f04231eaa51d18e
Aaron <aagavin@gmail.com>
2016-09-29 22:53:13 -0400

    add background and started script

---
+++ Assignment1_AaronFernandes/Assets/_Scripts/backgountController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgountController : MonoBehaviour {
+	// privates
+	private int _speed;
+	private Transform _transform;
+
+	// Use this for initialization
+	void Start () {
+		this._transform = this.GetComponent<Transform> ();
+
+		this._speed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 newPosition = this._transform.position;
+		newPosition.x -= this._speed;
+		this._transform.position = newPosition;
+	//TODO Make backgourd move
+	}
+}
